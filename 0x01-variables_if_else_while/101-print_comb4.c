bcd66b7f477118ad34d3c4401f1ff91a0f4a5a31
Alaa Abdo <alaa0elhosiny@gmail.com>
2022-12-09 14:03:40 -0800

    if_else_while Tasks

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -19,22 +19,18 @@ int main(void)
 	{
 		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 		{
-			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
-			{
-				putchar((digit1 % 10) + '0');
-				putchar((digit2 % 10) + '0');
-				putchar((digit3 % 10) + '0');
-
-				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
-					continue;
-				
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
-
-	putchar('\n');
-
-	return (0);
+for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+{
+putchar((digit1 % 10) + '0');
+putchar((digit2 % 10) + '0');
+putchar((digit3 % 10) + '0');
+if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
 }
