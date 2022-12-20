68a1c0abf54d3869c151b178ac56124feffd5231
Alaa Elhosiny <117745808+AlaaElhosiny@users.noreply.github.com>
2022-12-20 03:36:56 +0200

    Add files via upload

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,4 +9,15 @@
 */
 void reset_to_98(int *n);
 void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void swap_int(int *a, int *b);
+void swap_char(char *a, char *b);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _putchar(char c);
+int _strlen(char *s);
+int _atoi(char *s);
 #endif /*MAIN_H*/
