0c8ee8248e9b9c3568626c06965f6c2f1bd7482b
Alaa Elhosiny <117745808+AlaaElhosiny@users.noreply.github.com>
2023-01-02 20:32:48 +0200

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,9 +10,9 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-for (unsigned int i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
-	return (s);
+    for (unsigned int i = 0; i < n; i++)
+ {
+        s[i] = b;
+    }
+    return s;
 }
